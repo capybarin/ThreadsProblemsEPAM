bc76985daebca58bf3c60935b387dc5b885b84f6
capybarin <vladibzd@gmail.com>
2019-12-17 12:17:08 +0200

    problem 9.1 has been refactored

---
+++ src/com/company/CountToBomb.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class CountToBomb extends Thread {
+    public void run(){
+        for (int i = 10; i > 0; --i){
+            System.out.println(i);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Bomb");
+    }
+}
